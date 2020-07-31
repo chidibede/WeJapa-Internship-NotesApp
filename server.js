277e1ef7dcad9131111f08c9498c1aa9db24ff62
chidibede <chidibede@gmail.com>
2020-07-31 12:26:05 +0100

    Added ability to store notes locally and also sort them into directories

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const Note = require("./models/notes");
 const url = require("url");
+const fs = require("fs");
 
 mongoose.connect(
   process.env.MONGO_URI,
@@ -35,7 +36,216 @@ const server = http.createServer(async (req, res) => {
             res.end();
           }
         });
-      } else {
+      } else if (urlpath === "/notes/sortByCategories/Personal") {
+        await Note.find(
+          { categories: urlpath.split("/")[3] },
+          (error, data) => {
+            if (error) {
+              console.log("Error retrieving notes", error);
+            } else {
+              let noteTitleArray = [];
+              data.forEach((item) => {
+                noteTitleArray.push(item.title.replace(/\s+/g, "_"));
+              });
+
+              if (!fs.existsSync("notes_folder/Personal")) {
+                fs.mkdirSync("notes_folder/Personal");
+                for (let i = 0; i < noteTitleArray.length; i++) {
+                  fs.rename(
+                    "notes_folder/" + noteTitleArray[i] + ".txt",
+                    "notes_folder/Personal/" + noteTitleArray[i] + ".txt",
+                    (err) => {
+                      if (err) {
+                        console.log(err);
+                      } else {
+                        console.log("file sorted successfully");
+                      }
+                    }
+                  );
+                }
+              } else {
+                for (let i = 0; i < noteTitleArray.length; i++) {
+                  fs.rename(
+                    "notes_folder/" + noteTitleArray[i] + ".txt",
+                    "notes_folder/Personal/" + noteTitleArray[i] + ".txt",
+                    (err) => {
+                      if (err) {
+                        console.log(err);
+                      } else {
+                        console.log("file sorted successfully");
+                      }
+                    }
+                  );
+                }
+              }
+
+              res.writeHead(200, { "content-type": "application/json" });
+              res.write(JSON.stringify(data));
+              res.end();
+            }
+          }
+        );
+      }  else if (urlpath === "/notes/sortByCategories/Study") {
+        await Note.find(
+          { categories: urlpath.split("/")[3] },
+          (error, data) => {
+            if (error) {
+              console.log("Error retrieving notes", error);
+            } else {
+              let noteTitleArray = [];
+              data.forEach((item) => {
+                noteTitleArray.push(item.title.replace(/\s+/g, "_"));
+              });
+
+              if (!fs.existsSync("notes_folder/Study")) {
+                fs.mkdirSync("notes_folder/Study");
+                for (let i = 0; i < noteTitleArray.length; i++) {
+                  fs.rename(
+                    "notes_folder/" + noteTitleArray[i] + ".txt",
+                    "notes_folder/Study/" + noteTitleArray[i] + ".txt",
+                    (err) => {
+                      if (err) {
+                        console.log(err);
+                      } else {
+                        console.log("file sorted successfully");
+                      }
+                    }
+                  );
+                }
+              } else {
+                for (let i = 0; i < noteTitleArray.length; i++) {
+                  fs.rename(
+                    "notes_folder/" + noteTitleArray[i] + ".txt",
+                    "notes_folder/Study/" + noteTitleArray[i] + ".txt",
+                    (err) => {
+                      if (err) {
+                        console.log(err);
+                      } else {
+                        console.log("file sorted successfully");
+                      }
+                    }
+                  );
+                }
+              }
+
+              res.writeHead(200, { "content-type": "application/json" });
+              res.write(JSON.stringify(data));
+              res.end();
+            }
+          }
+        );
+      } else if (urlpath === "/notes/sortByCategories/Work") {
+        await Note.find(
+          { categories: urlpath.split("/")[3] },
+          (error, data) => {
+            if (error) {
+              console.log("Error retrieving notes", error);
+            } else {
+              res.writeHead(200, { "content-type": "application/json" });
+              res.write(JSON.stringify(data));
+              res.end();
+            }
+          }
+        );
+      } else if (urlpath === "/notes/sortByCategories/Work") {
+        await Note.find(
+          { categories: urlpath.split("/")[3] },
+          (error, data) => {
+            if (error) {
+              console.log("Error retrieving notes", error);
+            } else {
+              let noteTitleArray = [];
+              data.forEach((item) => {
+                noteTitleArray.push(item.title.replace(/\s+/g, "_"));
+              });
+
+              if (!fs.existsSync("notes_folder/Work")) {
+                fs.mkdirSync("notes_folder/Work");
+                for (let i = 0; i < noteTitleArray.length; i++) {
+                  fs.rename(
+                    "notes_folder/" + noteTitleArray[i] + ".txt",
+                    "notes_folder/Work/" + noteTitleArray[i] + ".txt",
+                    (err) => {
+                      if (err) {
+                        console.log(err);
+                      } else {
+                        console.log("file sorted successfully");
+                      }
+                    }
+                  );
+                }
+              } else {
+                for (let i = 0; i < noteTitleArray.length; i++) {
+                  fs.rename(
+                    "notes_folder/" + noteTitleArray[i] + ".txt",
+                    "notes_folder/Work/" + noteTitleArray[i] + ".txt",
+                    (err) => {
+                      if (err) {
+                        console.log(err);
+                      } else {
+                        console.log("file sorted successfully");
+                      }
+                    }
+                  );
+                }
+              }
+
+              res.writeHead(200, { "content-type": "application/json" });
+              res.write(JSON.stringify(data));
+              res.end();
+            }
+          }
+        );
+      } else if (urlpath === "/notes/sortByCategories/Others") {
+        await Note.find(
+          { categories: urlpath.split("/")[3] },
+          (error, data) => {
+            if (error) {
+              console.log("Error retrieving notes", error);
+            } else {
+              let noteTitleArray = [];
+              data.forEach((item) => {
+                noteTitleArray.push(item.title.replace(/\s+/g, "_"));
+              });
+
+              if (!fs.existsSync("notes_folder/Others")) {
+                fs.mkdirSync("notes_folder/Others");
+                for (let i = 0; i < noteTitleArray.length; i++) {
+                  fs.rename(
+                    "notes_folder/" + noteTitleArray[i] + ".txt",
+                    "notes_folder/Others/" + noteTitleArray[i] + ".txt",
+                    (err) => {
+                      if (err) {
+                        console.log(err);
+                      } else {
+                        console.log("file sorted successfully");
+                      }
+                    }
+                  );
+                }
+              } else {
+                for (let i = 0; i < noteTitleArray.length; i++) {
+                  fs.rename(
+                    "notes_folder/" + noteTitleArray[i] + ".txt",
+                    "notes_folder/Others/" + noteTitleArray[i] + ".txt",
+                    (err) => {
+                      if (err) {
+                        console.log(err);
+                      } else {
+                        console.log("file sorted successfully");
+                      }
+                    }
+                  );
+                }
+              }
+
+              res.writeHead(200, { "content-type": "application/json" });
+              res.write(JSON.stringify(data));
+              res.end();
+            }
+          }
+        );
+      }  else {
         let noteIdPattern = "[a-zA-Z0-9]+";
         let pattern = new RegExp("/notes/" + noteIdPattern);
         if (pattern.test(urlpath)) {
@@ -69,24 +279,45 @@ const server = http.createServer(async (req, res) => {
         });
 
         req.on("end", async () => {
-          try {
-            let postBody = JSON.parse(body);
-            let notes = Note(postBody);
-            await notes.save((error, data) => {
-              if (error) {
-                console.log("Error creating note", error);
+          let postBody = JSON.parse(body);
+          let notes = Note(postBody);
+          await notes.save((error, data) => {
+            if (error) {
+              console.log("Error creating note", error);
+            } else {
+              let response = {
+                data: data,
+              };
+              let noteTitle = data.title.replace(/\s+/g, "_");
+              console.log(noteTitle);
+              res.writeHead(200, { "content-type": "application/json" });
+              res.write(JSON.stringify(response));
+              res.end();
+
+              if (fs.existsSync("notes_folder")) {
+                fs.writeFile(
+                  "notes_folder/" + noteTitle + ".txt",
+                  data.content,
+                  (err) => {
+                    if (err) {
+                      console.log(err);
+                    }
+                  }
+                );
               } else {
-                let response = {
-                  data: data,
-                };
-                res.writeHead(200, { "content-type": "application/json" });
-                res.write(JSON.stringify(response));
-                res.end();
+                fs.mkdirSync("notes_folder");
+                fs.writeFile(
+                  "notes_folder/" + noteTitle + ".txt",
+                  data.content,
+                  (err) => {
+                    if (err) {
+                      console.log(err);
+                    }
+                  }
+                );
               }
-            });
-          } catch (err) {
-            console.log("Error " + err);
-          }
+            }
+          });
         });
       } else {
         res.writeHead(401, { "content-type": "text/plain" });
@@ -94,7 +325,7 @@ const server = http.createServer(async (req, res) => {
         res.end();
       }
       break;
-    
+
     case "PUT":
       if (urlpath === "/") {
         res.writeHead(200, { "content-type": "application/json" });
@@ -107,33 +338,37 @@ const server = http.createServer(async (req, res) => {
           pattern = new RegExp(noteIdPattern);
           let noteIdObj = pattern.exec(urlpath);
           let id = noteIdObj.input.split("/")[2];
-          let filter = {_id: id}
+          let filter = { _id: id };
           let body = "";
           req.on("data", (chunk) => {
             body += chunk;
           });
 
           req.on("end", async () => {
-              let update = JSON.parse(body);
-              await Note.findOneAndUpdate(filter, update, {useFindAndModify: false}, (error, data) => {
+            let update = JSON.parse(body);
+            await Note.findOneAndUpdate(
+              filter,
+              update,
+              { useFindAndModify: false },
+              (error, data) => {
                 if (error) {
                   console.log("Error creating note", error);
                 } else {
                   let response = {
                     updatedData: update,
-                    oldData: data
+                    oldData: data,
                   };
                   res.writeHead(200, { "content-type": "application/json" });
                   res.write(JSON.stringify(response));
                   res.end();
                 }
-              });
-           
+              }
+            );
           });
         }
       }
       break;
-    
+
     case "DELETE":
       if (urlpath === "/") {
         res.writeHead(200, { "content-type": "application/json" });
@@ -153,14 +388,13 @@ const server = http.createServer(async (req, res) => {
             } else {
               let response = {
                 message: "Deleted Successfully",
-                oldData: data
+                oldData: data,
               };
               res.writeHead(200, { "content-type": "application/json" });
               res.write(JSON.stringify(response));
               res.end();
             }
           });
-    
         }
       }
       break;
