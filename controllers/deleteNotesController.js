3442ac8cfa1dcb9cbd2bd350c21c7d357f37a8ce
chidibede <chidibede@gmail.com>
2020-08-08 21:28:21 +0100

    Ability to delete notes from directories

--- controllers/deleteNotesController.js
+++ controllers/deleteNotesController.js
@@ -10,11 +10,28 @@ module.exports = delete_requests = {
           pattern = new RegExp(noteIdPattern);
           let noteIdObj = pattern.exec(req.url);
           let id = noteIdObj.input.split("/")[2];
-
+          noteArray = []
+          await Note.findById(id, (error, data) => {
+            if (error){
+              console.log("Error finding note", error);
+            } else {
+              if (fs.existsSync(`notes_folder/${data.categories}`)) {
+                const title = data.title.replace(/\s+/g, "_")
+                fs.unlinkSync(`notes_folder/${data.categories}/${title}.txt`)
+                const directory = fs.readdirSync(`notes_folder/${data.categories}`)
+                if(!directory.length){
+                  fs.rmdirSync(`notes_folder/${data.categories}`)
+                }
+           
+              }
+            }
+          })
+         
           await Note.findByIdAndDelete(id, (error, data) => {
             if (error) {
               console.log("Error creating note", error);
             } else {
+              
               let response = {
                 message: "Deleted Successfully",
                 oldData: data,
